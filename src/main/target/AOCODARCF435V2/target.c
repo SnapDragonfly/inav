0eb1ca358e9b7671a980bc0354a1809d75ee9d72
daniel.li <lida_mail@163.com>
2023-10-24 15:06:26 +0800

    Add target AOCODARCF435V2, baro chip not found yet.

---
+++ src/main/target/AOCODARCF435V2/target.c
@@ -0,0 +1,45 @@
+/*
+ * This file is part of INAV.
+ *
+ * INAV is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * INAV is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with INAV.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+#include <stdint.h>
+
+#include "platform.h"
+#include "drivers/io.h"
+#include "drivers/timer.h"
+#include "drivers/pwm_mapping.h"
+#include "drivers/bus.h"
+
+timerHardware_t timerHardware[] = {
+    DEF_TIM(TMR2, CH4, PA3,   TIM_USE_ANY |TIM_USE_PPM, 0, 6 ),      // PPM IN
+
+    DEF_TIM(TMR3, CH1, PC6,   TIM_USE_OUTPUT_AUTO,      0, 3 ),      // S1 
+    DEF_TIM(TMR3, CH2, PC7,   TIM_USE_OUTPUT_AUTO,      0, 1 ),      // S2
+    DEF_TIM(TMR8, CH3, PC8,   TIM_USE_OUTPUT_AUTO,      0, 2 ),      // S3
+    DEF_TIM(TMR8, CH4, PC9,   TIM_USE_OUTPUT_AUTO,      0, 0 ),      // S4
+
+    DEF_TIM(TMR2, CH1, PA15,  TIM_USE_OUTPUT_AUTO,      0, 11),     // S5
+    DEF_TIM(TMR1, CH1, PA8,   TIM_USE_OUTPUT_AUTO,      0, 10),     // S6
+
+    DEF_TIM(TMR2, CH3, PB10,  TIM_USE_OUTPUT_AUTO,      0, 8 ),      // S7
+    DEF_TIM(TMR5, CH4, PB11,  TIM_USE_OUTPUT_AUTO,      0, 9 ),      // S8
+
+    DEF_TIM(TMR3, CH4, PB1,   TIM_USE_ANY |TIM_USE_LED, 0, 9 ),      // LED_STRIP
+};
+
+
+const int timerHardwareCount = sizeof(timerHardware) / sizeof(timerHardware[0]);
